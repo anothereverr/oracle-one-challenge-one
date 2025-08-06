51c54236508e89ccac4ff578a61c137b54baf84d
Daniel Mendoza <daniel.mendoza.caro@gmail.com>
2025-08-05 21:01:20 -0400

    feat: fix some small issues, add function to pick a winner, add function to clear ui fields

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let secretFriendArray = [];
+let friendNameInput = 'amigo';
+let frinedListOutput = 'listaAmigos';
 
 function addSecretFriend() {
-    const name = document.getElementById('amigo').value;
+    const name = document.getElementById(friendNameInput).value;
 
     if (typeof name !== 'string' || name.trim() === '') {
         alert('El nombre no puede ser vacio!');
@@ -12,13 +14,30 @@ function addSecretFriend() {
     secretFriendArray.push(name.trim());
     console.log(secretFriendArray);
     addToList(name);
+    clearInput(friendNameInput);
+}
+
+function clearInput(input) {
+    document.getElementById(input).value = '';    
+}
+
+function clearList(list) {
+    document.getElementById(list).innerHTML = '';
 }
 
 function addToList(name) {
-    const ul = document.getElementById('listaAmigos');
+    const ul = document.getElementById(frinedListOutput);
     const li = document.createElement('li');
     li.innerText = `Concursante: ${name}`;    
     ul.appendChild(li);
 }
 
+function pickFriend() {
+    if (!Array.isArray(secretFriendArray) || secretFriendArray.length <= 1) {
+        aler("Debe tener al menos dos amigos para sortear");
+    }
 
+    const randomIndex = Math.floor(Math.random() * secretFriendArray.length);
+    alert("El amigo secreto es" + secretFriendArray[randomIndex]);
+    clearList(frinedListOutput);
+}

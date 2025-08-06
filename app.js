52710613e6bcb48c536716119a67e3dd08018115
Daniel Mendoza <daniel.mendoza.caro@gmail.com>
2025-08-05 20:54:29 -0400

    feat: add funtion to add friends and show them in a list

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let secretFriendArray = [];
+
+function addSecretFriend() {
+    const name = document.getElementById('amigo').value;
+
+    if (typeof name !== 'string' || name.trim() === '') {
+        alert('El nombre no puede ser vacio!');
+        return;
+    }
+    secretFriendArray.push(name.trim());
+    console.log(secretFriendArray);
+    addToList(name);
+}
+
+function addToList(name) {
+    const ul = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.innerText = `Concursante: ${name}`;    
+    ul.appendChild(li);
+}
+
+
